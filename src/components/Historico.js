a08c4959409a77e490b3aad36b8dbe69e0590cd0
gustavo-de-assis <gustavo.siqueira.421@gmail.com>
2022-10-23 13:04:42 -0300

    layout básico historico

--- src/components/Historico.js
+++ src/components/Historico.js
@@ -1,3 +1,12 @@
+import styled from "styled-components"
+import Footer from "./Footer"
+import Header from "./Header"
+
 export default function Historico(){
-    
+    return(
+        <>
+        <Header/>
+        <Footer/>
+        </>
+    )
 }
