ff3e3bd8ebc8502ee3b44d3cec7a2cedc2acd4e0
gustavo-de-assis <gustavo.siqueira.421@gmail.com>
2022-10-23 10:33:02 -0300

    criação dos componentes principais

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "../assets/GlobalStyle";
 import Cadastro from "./Cadastro";
+import Habitos from "./Habitos";
+import Historico from "./Historico";
+import Hoje from "./Hoje";
 import Login from "./Login";
+
 
 export default function  App(){
     return(
       <BrowserRouter>
         <GlobalStyle/>
         <Routes>
-            <Route path="/" element={<Login />}/>
+            {/*<Route path="/" element={<Login />}/>*/}
             <Route path="/cadastro" element={<Cadastro />}/>
+            <Route path="/habitos" element={<Habitos />}/>
+            <Route path="/hoje" element={<Hoje/>}/>
+            <Route path="/historico" element={<Historico/>}/>
         </Routes>
       </BrowserRouter>
     )
