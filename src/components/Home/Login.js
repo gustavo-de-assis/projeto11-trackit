bf9cb2ea21bcc2c3c9057cb1f35a72b21ef30ba3
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-10-24 02:59:42 -0300

    secao de login ver 1.0

--- src/components/Home/Login.js
+++ src/components/Home/Login.js
@@ -1,24 +1,62 @@
+import axios from "axios";
+import { useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import logo from "../../assets/img/logo.png"
 
+
 export default  function Login(){
+    const [loginInfo, setLoginInfo] = useState({email:"",password:""})
+    const [loginAttempt, setLoginAttempt]= useState(false);
+    const URL ='https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login';
+
+    function logUser(e){
+        e.preventDefault();
+
+        const log = {...loginInfo}
+        console.log(loginInfo);
+        
+        setLoginAttempt(true);
+
+         axios.post(URL, log)
+        .then((ans)=>console.log('Logado com sucesso', ans.data))
+        .catch((err)=>{
+                alert("Usuário não cadastrado!")
+                console.log(err.response.data.message)
+                setLoginAttempt(false);
+            }
+        ) 
+                
+    }
+
+    function logHandler(e){
+        const {name, value} = e.target;
+        setLoginInfo({ ...loginInfo, [name]:value});
+    }
+
     return(
         <Container>
             <img src={logo} alt="logo"/>
 
-            <form onSubmit={()=>alert("oi")}>
+            <form onSubmit={logUser} disabled={loginAttempt}>
                 <input 
                     name="email"
+                    value={loginInfo.email}
+                    onChange={logHandler}
                     type="email"
                     placeholder="E-mail"
+                    required
                     />
                 <input
-                    name="senha"
+                    name="password"
+                    value={loginInfo.password}
+                    onChange={logHandler}
                     type="password"
                     placeholder="Senha"
+                    required
                     />
-                <button type="submit">Entrar</button>
+                <button type="submit" disabled={loginAttempt}>{loginAttempt ? <ThreeDots color={"white"}/>:'Entrar'}</button>
             </form>
             <Link to={"/cadastro"}>Não tem uma conta? Cadastre-se!</Link>
 
@@ -49,6 +87,19 @@ const Container = styled.div`
         button{
             background-color: #52B6FF;
             color: #fff;
+            
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            
+            :hover{
+                cursor: pointer;
+                opacity: 0.8;
+            }
+            :disabled{
+                opacity: 0.8;
+            }
         }
     }
     a{
